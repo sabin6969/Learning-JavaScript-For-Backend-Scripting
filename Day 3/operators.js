7ab00cdbbcfb93c515daf0315c5af41ffc75e723
Sabin Poudel <sabinpoudel6969@gmail.com>
2024-01-05 20:22:52 +0530

    learned about arithmetic and ternary operators

---
+++ Day 3/operators.js
@@ -0,0 +1,28 @@
+// Operators in java script
+
+// arithmetic operator
+
+let number1 = 11;
+let number2 = 4;
+let sum = number1+number2;
+console.log(sum);
+
+let diff = number1-number2;
+console.log(diff);
+
+let mult = number1*number2;
+console.log(mult);
+
+let floatDivision = number1/number2;
+console.log(floatDivision);
+
+console.log(Math.floor(floatDivision)); // gives integer division
+
+
+
+// ternary operator
+// short form of if else statement
+// conditional statement? true?false
+let number =32;
+let result = number%2==0? "Even":"Odd";
+console.log(result);
