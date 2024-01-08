4c8c06b7fbaae5c5774cb540e018752f804e37c2
Sabin Poudel <sabinpoudel6969@gmail.com>
2024-01-08 22:08:02 +0530

    learned about some wierd behaviour of js

---
+++ Day 5/wierd.js
@@ -0,0 +1,7 @@
+console.log(typeof null ); // object
+console.log(typeof Date); // function
+console.log(typeof BigInt); // function
+// we cannot use new keyword in front of BigInt like we used to do for String, Number, Boolean
+
+let number =  Number(12.12); // primitive
+console.log(number);
