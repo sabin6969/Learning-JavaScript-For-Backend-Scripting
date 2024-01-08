83c92d1bbfdafe29fcb5bcdf0434a9c98be99b70
Sabin Poudel <sabinpoudel6969@gmail.com>
2024-01-08 21:55:42 +0530

    learned about date and math builtins

---
+++ Day 5/date.js
@@ -0,0 +1,9 @@
+// wrapper object 
+// wrapper object provides additional functionality 
+let todaysDate = new Date();
+console.log(todaysDate);
+console.log(todaysDate.getFullYear()); // 2024
+console.log(todaysDate.getMonth()); // 0 for jan 
+console.log(todaysDate.getDate()); 
+console.log(todaysDate.getDay()); // for sunday 0
+console.log(new Date("2023-10-11"));
---
+++ Day 5/math.js
@@ -0,0 +1,9 @@
+// Math is a built in Object in js
+
+console.log(Math.floor(12.12)); // 12
+console.log(Math.ceil(12.21));; //13
+console.log(Math.ceil(12.0001)); //13
+console.log(Math.random());// retruns number in between 0 and 1
+console.log(Math.max(1,2,3,4));// 4
+console.log(Math.min(-10,2,2,-111));// -111
+console.log(Math.abs(-12)); // 12
