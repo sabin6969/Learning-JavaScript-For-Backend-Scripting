24555393236aee3882eee65bf07c31717f1aa96e
Sabin Poudel <sabinpoudel6969@gmail.com>
2024-01-24 17:20:45 +0530

    learned about array like object and iterable objects

---
+++ Day 14/array_like_object.js
@@ -0,0 +1,11 @@
+// those object which have length property
+// and can be accessed using index
+// string and array are example of such object
+let naam = "Sabin";
+console.log(naam.length);
+console.log(naam[0]);
+
+
+let numbers = [1,2,3,4,5];
+console.log(numbers.length);
+console.log(numbers[2]);
---
+++ Day 14/iterable_object.js
@@ -0,0 +1,14 @@
+// those objects which can be iterated through for of loop
+// arrays, strings are iterable objects
+
+
+let naam = "Sabin Poudel";
+for(let char of naam){
+    console.log(naam);
+}
+
+console.log();
+let names = ["Sabin","Raj","Smith","Biren","Padam"];
+for(let name of names){
+    console.log(name);
+}
